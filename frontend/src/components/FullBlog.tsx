aa2d445fbe9604afd3bcb0d394c0d97f1ba3f8b9
RITESH SHARMA <ritesh@RITESHs-MacBook-Air.local>
2025-03-27 04:23:20 +0530

    third update

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -3,13 +3,13 @@ import { Blog } from "../hooks"
 export const FullBlog=({blog}:{blog:Blog})=>{
     return <div>
         <AppBar/>
-        <div className="grid grid-cols-12 px-20 pt-10">
-            <div className="col-span-8">
+        <div className="grid grid-cols-1 md:grid-cols-12 px-20 pt-10 grid">
+            <div className=" col-span-1 md:col-span-8">
                 <div className="text-5xl font-extrabold">{blog.title}</div>
                 <div className="pt-2 text-slate-400"> Published on 02 Dec 2023</div>
                 <div className="text-xl pt-4">{blog.content}</div>        
             </div>
-            <div className="col-span-4">
+            <div className="col-span-1 md:col-span-4">
                 Author
                 <div className="text-2xl font-bold">
                     {blog.author.name || "Anonymous"}
